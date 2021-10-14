b56177c3e3d2a84dfe112110112cdc3d1f005913
Matteo Pisani <matteo.pisani.roma@gmail.com>
2021-10-14 18:05:27 +0200

    Add project files.

---
+++ vs-2019-new-git-experience/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_2019_new_git_experience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
